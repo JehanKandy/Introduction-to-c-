f341a985d6ae84dcdb450f9179db43d3e3be013b
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-11-06 07:33:56 +0530

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,33 @@
+//c++ program add 3 numbers
+
+#include <bits/stdc++, h>
+using namespace std;
+
+int main()
+{
+
+//variables to input 3 numbers
+int n1,n2,n32;
+//variable to store the result sum
+int sum;
+
+//take 3 number as the input
+cout << "Enter 1st Number : ";
+cin >> n1;
+cout << "" << num1 << end1;
+
+cout << "Enter 2nd Number : ";
+cin >> n2;
+cout << "" << num2 << end2;
+
+cout << "Enter 3rd Number : ";
+cin >> n3;
+cout << "" << num3 << end3;
+
+//print output
+sum = n1 + n2 + n3;
+cout << "\n Sum of the 3 number is : "sum;
+
+return 0;
+
+}
